c15b8118a75b7d875b846f49a2d324ad6c4996a9
Madhukar Kumar <madhukar.kumar@gmail.com>
2024-05-11 16:53:31 -0700

    New LinkedIn Page
    Added LinkedIn Posts page. Right now it is static

--- components/medium.jsx
+++ components/medium.jsx
@@ -3,16 +3,18 @@ import Image from 'next/image';
 import '../app/globals.css';
 import Link from "next/link";
 
-const Medium = ({ username }) => {
+const Medium = ({username}) => {
     return (
-        <div className="card card-bordered h-52 rounded-lg max-w-full bg-base-100 items-center pt-5 pb-8 shadow-2xl ">
-            <Image src="/images/medium-icon.svg" alt="Medium logo" width={50} height={50} />
+        <div
+            className="card bordered h-52 rounded-lg max-w-full bg-base-100 items-center pt-5 pb-8 shadow-2xl">
+            <Image src="/images/medium-icon.svg" alt="Medium logo" width={50} height={50}/>
             <div className="font-sans text-[28px] font-medium">
                 <Link href="https://madhukarkumar.medium.com" target="_blank"> Medium </Link>
             </div>
             <div className="card-body">
                 <Link
-                    href="https://madhukarkumar.medium.com" target="_blank" className="mb-16 text-left font-sans text-[18px]">
+                    href="https://madhukarkumar.medium.com" target="_blank"
+                    className="mb-16 text-left font-sans text-[18px] overflow-hidden text-overflow-ellipsis">
                     Marketing, PLG and Tech
                 </Link>
             </div>
