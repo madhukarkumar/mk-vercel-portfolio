c084f7a2e54ff6dc25f6cb6e2d20f7dea312675a
Madhukar Kumar <madhukar.kumar@gmail.com>
2025-02-20 15:23:36 -0800

    menu fixes

--- components/header-menu.tsx
+++ components/header-menu.tsx
@@ -1,6 +1,6 @@
 export function HeaderMenu() {
   return (
-    <nav className="sticky top-10 z-50 w-full pointer-events-none">
+    <nav className="sticky top-2 z-50 w-full pointer-events-none">
       <div className="container max-w-[500px] mx-auto pointer-events-auto">
         <div className="h-16 bg-background/70 backdrop-blur-md shadow-lg rounded-full border-[2px]">
           <div className="flex justify-center gap-6 flex-row h-full items-center">
@@ -9,23 +9,20 @@ export function HeaderMenu() {
             </a>
             <div className="hidden md:flex items-center justify-center space-x-6">
               <a
-                href="/essays"
+                href="/blogs"
                 className="text-sm font-medium transition-colors hover:text-primary relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-primary after:transition-all hover:after:w-full"
               >
                 Essays
               </a>
               <a
-                href="/work"
+                href="https://github.com/madhukarkumar"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="text-sm font-medium transition-colors hover:text-primary relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-primary after:transition-all hover:after:w-full"
               >
                 Work
               </a>
-              <a
-                href="/resume"
-                className="text-sm font-medium transition-colors hover:text-primary relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-primary after:transition-all hover:after:w-full"
-              >
-                Resume
-              </a>
+              
             </div>
           </div>
         </div>
